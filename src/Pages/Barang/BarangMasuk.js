ccdba0f193334896baa9599755a0b834672e6c09
WILMAR <resnuwilmar1@gmail.com>
2024-06-12 12:53:08 +0700

    penamaan search bar

--- src/Pages/Barang/BarangMasuk.js
+++ src/Pages/Barang/BarangMasuk.js
@@ -48,7 +48,7 @@ const BarangMasuk = () => {
         </div>
         <input 
           type="text" 
-          placeholder="Search by transaction ID or type" 
+          placeholder="Search..." 
           value={filter} 
           onChange={handleSearchChange} 
           className="px-3 py-2 border rounded shadow-sm focus:outline-none"
